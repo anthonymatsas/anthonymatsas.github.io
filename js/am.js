3851b067711ece286c11041c304808bc2c44345a
Tony Matsas <tonymatsas@gmail.com>
2018-09-29 23:49:01 -0500

    Cleanup: removed jquery, gravatar and bloated css

--- js/am.js
+++ js/am.js
@@ -1,30 +1,11 @@
-$(document).ready(function() {
-	var $gravatar = $('#gravatar'),
-		$projectsLink = $('#projectsLink'),
-		$projects = $('#projects'),
-		$projectsClose = $('#closeButton'),
-		$copyright = $('#copyright');
+window.onload = function() {
+	var copyright = document.querySelector('#copyright');
 
 	setCopyrightYear();
-
-	$gravatar.empty()
-		.append($.gravatar('tony@anthonymatsas.com'));
-
-	$projectsLink.on('click', function(event) {
-		event.preventDefault();
-
-		$projects.show();
-	});
-
-	$projectsClose.on('click', function(event) {
-		event.preventDefault();
-
-		$projects.hide();
-	});
 
 	function setCopyrightYear() {
 		let year = new Date().getFullYear();
 
-		$copyright.append(year);
+		copyright.append(year);
 	}
-});
+}
