6cee6bc5852f30c25ae0488f0c34fd21e233fd21
Tony Matsas <tonymatsas@gmail.com>
2019-07-22 20:07:21 -0500

    Removed not needed js

--- js/am.js
+++ js/am.js
@@ -1,11 +1,4 @@
 window.onload = function() {
 	var copyright = document.querySelector('#copyright');
-
-	setCopyrightYear();
-
-	function setCopyrightYear() {
-		let year = new Date().getFullYear();
-
-		copyright.append(year);
-	}
+	copyright.append(new Date().getFullYear());
 }
